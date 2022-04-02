7c7311278d79bc7c93fc0afb6f8a70d1f0f6da92
Muhamed Aashiq <muhamedaashiqtce@gmail.com>
2022-04-02 13:53:20 -0400

    Init react app

---
+++ gk-coin-client/src/App.js
@@ -0,0 +1,15 @@
+import './common-styles.scss'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <div className="gk-coin-wrapper flex flex-col">
+                <div>Section 1</div>
+                <div>Section 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default App
